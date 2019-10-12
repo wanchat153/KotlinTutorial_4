94528c86da11e26b344a8ee90994a2f561968ae6
Wanchat <wanchatgame@gmail.com>
2019-10-12 20:47:02 +0700

    1. สร้าง Main.kt ลบ MainActivity.kt
    2.
    println My first Kotlin program
    
    3.
    สร้าง timSalary กับ monthly
    
    4.
    เปลี่ยนชื่อ timWeeklySalary กับ timsMonthlySalary
    สร้าง fruit apples + oranges
    
    5.
    เพิ่มเนื้อหา "xxx"
    
    6.
    เปลี่ยน var เป็น val และจัด code ใหม่
    
    7.
    //ทั้งหมด
    สร้าง args
    
    8.
    สร้าง Player, name, lives, level, score
    มีผู้เล่นชื่อ tim, louise, gr8, Ace
    
    9.
    สร้าง Weapon ใส่ axe, Spear, Sword กับ damageInfLicted
    
    10.
    จัดเรียงcodeใหม่
    
    11.
    สร้าง Loot.kt
    
    12.
    set redPotion
    
    13.
    เพิ่ม override fun ใน Loot และ Weapon

--- app/src/main/java/com/example/kotlintutorial_4/Player.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Player.kt
@@ -19,8 +19,7 @@ class Player (val name: String, var level: Int = 1, var lives: Int = 3, var scor
             lives: $lives
             level: $level
             score: $score
-            weapon: ${weapon.name}
-            damage: ${weapon.damageInfLicted}
+            weapon: ${weapon}
             """
     }
 
--- app/src/main/java/com/example/kotlintutorial_4/Weapon.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Weapon.kt
@@ -2,4 +2,7 @@ package com.example.kotlintutorial_4
 
 class Weapon(val name: String, var damageInfLicted: Int){
 
+    override fun toString(): String {
+        return "$name inflicts $damageInfLicted points of damage"
+    }
 }
