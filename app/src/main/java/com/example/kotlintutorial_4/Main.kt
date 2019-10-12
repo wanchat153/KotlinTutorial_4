2ac6cf7d3488aaf1f96b8400cde6a4627f9b154b
Wanchat <wanchatgame@gmail.com>
2019-10-12 19:09:21 +0700

    สร้าง Main.kt ลบ MainActivity.kt
    println My first Kotlin program
    
    สร้าง timSalary กับ monthly
    
    เปลี่ยนชื่อ timWeeklySalary กับ timsMonthlySalary
    สร้าง fruit apples + oranges
    
    เพิ่มเนื้อหา "xxx"
    
    เปลี่ยน var เป็น val และจัด code ใหม่
    
    //ทั้งหมด
    สร้าง args

--- app/src/main/java/com/example/kotlintutorial_4/Main.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Main.kt
@@ -1,30 +1,63 @@
 fun main(args: Array<String>){
-    println("Hello World")
-    println("My first Kotlin program")
+//    println("Hello World")
+//    println("My first Kotlin program")
+//
+//    val jim = "James T Kirk"
+//    val tim = "Tim Buchalka"
+//    println(tim)
+//
+//    val timsWeeklySalary: Int = 32
+//    val timsMonthlySalary = timsWeeklySalary * 4
+//    println("Tim' s $ weekly salary is $$timsWeeklySalary")
+//    println("Monthly, that comes to $timsMonthlySalary")
+//
+//    println()
+//
+//    val apples = 6
+//    val oranges = 5
+//    val fruit = apples - oranges
+//    println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
+//
+//    println("A quarter of the apples is ${apples / 4}")
+//
+//    println()
+//    val weeks = 234
+//    val years = weeks / 52.0
+//    println("$weeks weeks in $years  years")
+//
+//    println("My name is $tim")
+//    println("jim is $jim")
 
-    val jim = "James T Kirk"
-    val tim = "Tim Buchalka"
-    println(tim)
+//    var lives = 0
+//
+//    var isGameOver = (lives < 1)
+//    println(isGameOver)
+//
+//    if (isGameOver){
+//        println("Game over!")
+//    }else{
+//        println("You're still alive!")
+//    }
 
-    val timsWeeklySalary: Int = 32
-    val timsMonthlySalary = timsWeeklySalary * 4
-    println("Tim' s $ weekly salary is $$timsWeeklySalary")
-    println("Monthly, that comes to $timsMonthlySalary")
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("age is $age")
 
-    println()
+    val messenge: String
+    messenge = when {
+        age < 18 -> "You're too young to vote"
+        age == 100 -> "Congratulations"
+        else -> "You can vote"
+    }
 
-    val apples = 6
-    val oranges = 5
-    val fruit = apples - oranges
-    println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
+//    val messenge: String
+//    messenge = if (age < 18){
+//        "You're too young to vote"
+//    }else if (age == 100){
+//        "Congratulations"
+//    }else{
+//        "You can vote"
+//    }
 
-    println("A quarter of the apples is ${apples / 4}")
-
-    println()
-    val weeks = 234
-    val years = weeks / 52.0
-    println("$weeks weeks in $years  years")
-
-    println("My name is $tim")
-    println("jim is $jim")
+    println(messenge)
 }
