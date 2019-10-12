32fdbc3b8bd2461157225753ddd752ca73644427
Wanchat <wanchatgame@gmail.com>
2019-10-12 18:20:24 +0700

    สร้าง Main.kt ลบ MainActivity.kt
    println My first Kotlin program
    
    สร้าง timSalary กับ monthly
    
    เปลี่ยนชื่อ timWeeklySalary กับ timsMonthlySalary
    สร้าง fruit apples + oranges

--- app/src/main/java/com/example/kotlintutorial_4/Main.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Main.kt
@@ -6,8 +6,22 @@ fun main(args: Array<String>){
     tim = "32"
     println(tim)
 
-    var timSalary: Int = 32
-    var monthly: Int = timSalary * 4
-    println(timSalary)
-    println(monthly)
+    var timWeeklySalary: Int = 32
+    var timsMonthlySalary: Int = timWeeklySalary * 4
+    println(timWeeklySalary)
+    println(timsMonthlySalary)
+
+    println()
+
+    val apples: Int = 6
+    val oranges: Int = 5
+    var fruit: Int = apples + oranges
+    println(fruit)
+
+    println(apples / 4)
+
+    println()
+    val weeks: Int = 130
+    val years: Double = weeks / 52.0
+    println(years)
 }
