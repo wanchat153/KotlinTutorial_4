b8deb301341b743d7f29893e728d14b5796b1062
Wanchat <wanchatgame@gmail.com>
2019-10-12 20:13:50 +0700

    1. สร้าง Main.kt ลบ MainActivity.kt
    2.
    println My first Kotlin program
    
    3.
    สร้าง timSalary กับ monthly
    
    4.
    เปลี่ยนชื่อ timWeeklySalary กับ timsMonthlySalary
    สร้าง fruit apples + oranges
    
    5.
    เพิ่มเนื้อหา "xxx"
    
    6.
    เปลี่ยน var เป็น val และจัด code ใหม่
    
    7.
    //ทั้งหมด
    สร้าง args
    
    8.
    สร้าง Player, name, lives, level, score
    มีผู้เล่นชื่อ tim, louise, gr8, Ace
    
    9.
    สร้าง Weapon ใส่ axe, Spear, Sword กับ damageInfLicted
    
    10.
    จัดเรียงcodeใหม่

--- app/src/main/java/com/example/kotlintutorial_4/Main.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Main.kt
@@ -25,8 +25,13 @@ fun main(args: Array<String>){
     println(gr8.weapon.damageInfLicted)
 
     tim.weapon = Weapon("Sword", 10)
-    println(tim.weapon.name)
+//    println(tim.weapon.name)
+    tim.show()
+
+    louise.weapon = tim.weapon
+    louise.show()
 
     tim.weapon = Weapon("Spear", 14)
-    println(tim.weapon.name)
+//    println(tim.weapon.name)
+    tim.show()
 }
--- app/src/main/java/com/example/kotlintutorial_4/Player.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Player.kt
@@ -10,6 +10,8 @@ class Player (val name: String, var level: Int = 1, var lives: Int = 3, var scor
             lives: $lives
             level: $level
             score: $score
+            weapon: ${weapon.name}
+            damage: ${weapon.damageInfLicted}
             """)
     }
 }
