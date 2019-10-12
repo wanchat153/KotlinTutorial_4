dd444649d99a4b82f2b5f2e5ad496a0ec7d278e0
Wanchat <wanchatgame@gmail.com>
2019-10-12 21:10:38 +0700

    1. สร้าง Main.kt ลบ MainActivity.kt
    2.
    println My first Kotlin program
    
    3.
    สร้าง timSalary กับ monthly
    
    4.
    เปลี่ยนชื่อ timWeeklySalary กับ timsMonthlySalary
    สร้าง fruit apples + oranges
    
    5.
    เพิ่มเนื้อหา "xxx"
    
    6.
    เปลี่ยน var เป็น val และจัด code ใหม่
    
    7.
    //ทั้งหมด
    สร้าง args
    
    8.
    สร้าง Player, name, lives, level, score
    มีผู้เล่นชื่อ tim, louise, gr8, Ace
    
    9.
    สร้าง Weapon ใส่ axe, Spear, Sword กับ damageInfLicted
    
    10.
    จัดเรียงcodeใหม่
    
    11.
    สร้าง Loot.kt
    
    12.
    set redPotion
    
    13.
    เพิ่ม override fun ใน Loot และ Weapon
    
    14.
    //ทั้งหมด
    สอนการคำนวน

--- app/src/main/java/com/example/kotlintutorial_4/Main.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Main.kt
@@ -4,41 +4,57 @@ import com.example.kotlintutorial_4.Player
 import com.example.kotlintutorial_4.Weapon
 
 fun main(args: Array<String>){
-    val tim = Player("Tim")
-    tim.show()
+//    val tim = Player("Tim")
+//    tim.show()
+//
+//    val louise = Player("Louise", 5)
+//    louise.show()
+//
+//    val gr8 = Player("gr8", 4, 8)
+//    val one2wach = Player("Ace", 2, 5, 1000)
+//    gr8.show()
+//    one2wach.show()
+//    println(one2wach.weapon.name.toUpperCase())
+//    println(one2wach.weapon.damageInfLicted)
+//
+//    val axe = Weapon("Axe", 12)
+//    gr8.weapon = axe
+//    println(gr8.weapon.name)
+//    println(axe.name)
+//
+//    axe.damageInfLicted = 24
+//    println(axe.damageInfLicted)
+//    println(gr8.weapon.damageInfLicted)
+//
+//    tim.weapon = Weapon("Sword", 10)
+////    println(tim.weapon.name)
+//    tim.show()
+//
+//    louise.weapon = tim.weapon
+//    louise.show()
+//
+//    tim.weapon = Weapon("Spear", 14)
+////    println(tim.weapon.name)
+//    tim.show()
+//
+//    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
+//    tim.inventory.add(redPotion)
+//    val chesArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.00)
+//    tim.inventory.add(chesArmor)
+//    tim.showInventory()
+//
+//    tim.inventory.add(Loot("Ring of Protection +2", LootType.RING, 40.25))
+//    tim.inventory.add(Loot("Inventory Potion", LootType.POTION, 35.95))
+//    tim.showInventory()
+//
+//    println(tim.toString())
+    for (i in 10 downTo 0 step 2){
+        println("$i squared is ${i * i}")
+    }
 
-    val louise = Player("Louise", 5)
-    louise.show()
-
-    val gr8 = Player("gr8", 4, 8)
-    val one2wach = Player("Ace", 2, 5, 1000)
-    gr8.show()
-    one2wach.show()
-    println(one2wach.weapon.name.toUpperCase())
-    println(one2wach.weapon.damageInfLicted)
-
-    val axe = Weapon("Axe", 12)
-    gr8.weapon = axe
-    println(gr8.weapon.name)
-    println(axe.name)
-
-    axe.damageInfLicted = 24
-    println(axe.damageInfLicted)
-    println(gr8.weapon.damageInfLicted)
-
-    tim.weapon = Weapon("Sword", 10)
-//    println(tim.weapon.name)
-    tim.show()
-
-    louise.weapon = tim.weapon
-    louise.show()
-
-    tim.weapon = Weapon("Spear", 14)
-//    println(tim.weapon.name)
-    tim.show()
-
-    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
-    tim.inventory.add(redPotion)
-    tim.showInventory()
-    println(tim.toString())
+    for (value in 3..100 step 3){
+        if (value % 5 == 0){
+            println(value)
+        }
+    }
 }
--- app/src/main/java/com/example/kotlintutorial_4/Player.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Player.kt
@@ -24,8 +24,10 @@ class Player (val name: String, var level: Int = 1, var lives: Int = 3, var scor
     }
 
     fun showInventory(){
-        println("$name'$ Inventory")
-        println(inventory.get(0))
+        println("$name's Inventory")
+        for (item in inventory){
+            println(item)
+        }
         println("========================")
     }
 }
