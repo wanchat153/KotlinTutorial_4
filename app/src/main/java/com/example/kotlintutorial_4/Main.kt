1d0c76d06ab220c37661b8277d5930583e2aecc5
Wanchat <wanchatgame@gmail.com>
2019-10-12 18:12:09 +0700

    สร้าง Main.kt ลบ MainActivity.kt
    println My first Kotlin program
    
    สร้าง timSalary กับ monthly

--- app/src/main/java/com/example/kotlintutorial_4/Main.kt
+++ app/src/main/java/com/example/kotlintutorial_4/Main.kt
@@ -1,4 +1,13 @@
 fun main(args: Array<String>){
     println("Hello World")
     println("My first Kotlin program")
+
+    var tim: String
+    tim = "32"
+    println(tim)
+
+    var timSalary: Int = 32
+    var monthly: Int = timSalary * 4
+    println(timSalary)
+    println(monthly)
 }
